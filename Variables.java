8e4018191ef3807ea0f8a41b93590eb623cc84f2
Cyril <c.concas.ext@axione.fr>
2024-06-10 16:07:06 +0200

    First solution

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+ public static void main(String [] args)
+ {
+	 String filmTitle = "Indiana Jones et la Dernière Croisade";
+	 boolean isSeen = false;
+	 int year = 1989;
+	 float note = 8.9f;
+    System.out.println(filmTitle);
+	System.out.println(isSeen);
+	System.out.println(year);
+	System.out.println(note);
+ }
+}
